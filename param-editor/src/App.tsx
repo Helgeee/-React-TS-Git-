b7dc4ac40eeb509c4b953a3aecd9297a7902b90c
helgeee <olegvotoz@gmail.com>
2026-01-28 14:03:36 +0300

    update App  add import type

--- param-editor/src/App.tsx
+++ param-editor/src/App.tsx
@@ -1,7 +1,23 @@
+import type { Param, Model } from "./ParamEditor";
+import { ParamEditor } from "./ParamEditor";
+
+const params: Param[] = [
+  { id: 1, name: "Назначение", type: "string" },
+  { id: 2, name: "Длина", type: "string" },
+];
+
+const model: Model = {
+  paramValues: [
+    { paramId: 1, value: "повседневное" },
+    { paramId: 2, value: "макси" },
+  ],
+};
+
 function App() {
   return (
-    <div>
-      <h1>Param Editor</h1>
+    <div style={{ padding: 20 }}>
+      <h1>Редактор параметров</h1>
+      <ParamEditor params={params} model={model} />
     </div>
   );
 }
